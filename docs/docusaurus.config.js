427becd53d2f1eeeaa88da83a6d6f4f1f19cd746
Sam Zhang <samzhangjy@outlook.com>
2022-09-12 20:53:19 +0800

    chore: added google analytics

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -44,6 +44,9 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'G-FKBKNWQR0L',
+        },
       }),
     ],
   ],
